cc4ccc12c752abebdc4b01ef5ac801938e80ae40
weaveworld <cs.veg@weaveworld.org>
2019-04-29 06:31:40 +0200

    v0.14.190428

--- resource/app.js
+++ resource/app.js
@@ -6,23 +6,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 console.log('Server started: http://localhost:3000');
 
-const weaveworld=require('weaveworld');
-weaveworld.dir=__dirname+'/public';
-app.all('/*',weaveworld.route);    
+const w=require('weaveworld');
+w.dir=__dirname+'/public';
+app.all('/*',w.route);    
 
 
-weaveworld.op('hello').then((o)=>({message:'Hello '+(o.name||'World')+'!'}));
+w.op('hello').then((o)=>({message:'Hello '+(o.name||'World')+'!'}));
 
 /*
 const mysql = require('mysql'), connection = mysql.createConnection({
   host:'localhost', database:'xyz', user:'root', password:'',
 })
-weaveworld.connection=connection;
-weaveworld.sqlLog=console.log;
+w.connection=connection;
+w.sqlLog=console.log;
 
-weaveworld.op('eachX').sql("SELECT * FROM x");
-weaveworld.op('getX').then(o=>[o.id]).sql("SELECT * FROM x WHERE id=?").then(o=>o.length?o[0]:{});
-weaveworld.op('setX').then(o=>[o.f1,o.f2,o.id]).sql("UPDATE x SET f1=?,f2=? WHERE id=?");
-weaveworld.op('addX').sql("INSERT INTO x SET ?").then(o=>{id:o.insertId});
+w.op('eachX').sql("SELECT * FROM x");
+w.op('getX').then(o=>[o.id]).sql("SELECT * FROM x WHERE id=?").then(o=>o.length?o[0]:{});
+w.op('setX').then(o=>[o.f1,o.f2,o.id]).sql("UPDATE x SET f1=?,f2=? WHERE id=?");
+w.op('addX').sql("INSERT INTO x SET ?").then(o=>{id:o.insertId});
 */
 
